9ff883df07638afe88da62fac7bdea37b912df66
Smit Shah <smitshah24.ss@gmail.com>
2025-05-25 13:29:42 -0400

    removed converters

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,45 +61,6 @@ const LiveEditor = dynamic(() => import("../features/editor/LiveEditor"), {
   ssr: false,
 });
 
-const theme = createTheme({
-  autoContrast: true,
-  fontSmoothing: false,
-  respectReducedMotion: true,
-  cursorType: "pointer",
-  fontFamily:
-    'system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
-  defaultGradient: {
-    from: "#388cdb",
-    to: "#0f037f",
-    deg: 180,
-  },
-  primaryShade: 8,
-  colors: {
-    brightBlue: [
-      "#e6f2ff",
-      "#cee1ff",
-      "#9bc0ff",
-      "#649dff",
-      "#3980fe",
-      "#1d6dfe",
-      "#0964ff",
-      "#0054e4",
-      "#004acc",
-      "#003fb5",
-    ],
-  },
-  radius: {
-    lg: "12px",
-  },
-  components: {
-    Button: {
-      defaultProps: {
-        fw: 500,
-      },
-    },
-  },
-});
-
 const EditorPage = () => {
   const { query, isReady } = useRouter();
   const checkEditorSession = useFile(state => state.checkEditorSession);
